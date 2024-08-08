5877f86e9f603d9301746d0ea70f6d1e73e499f3
Palombo <dripalombo22@gmail.com>
2024-08-08 19:06:05 -0300

    feat: opções de menu no drawer e select

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,48 +1,102 @@
-import { Avatar, Box, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material"
-import imageSrc from './1717471943_124435_blog_3__quando_meu_filhote_deve_tomar_remedio_verme_.jpeg';
-import { useDrawerContext} from "../../contexts";
+import {
+  Avatar,
+  Box,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import imageSrc from "./1717471943_124435_blog_3__quando_meu_filhote_deve_tomar_remedio_verme_.jpeg";
+import { useDrawerContext } from "../../contexts";
+import { useMatch, useNavigate, useResolvedPath } from "react-router-dom";
 
+interface IListItemProps {
+    label: string;
+    icon: string;
+    to: string;
+    onClick: (() => void) | undefined;
+}
 
+const ListItemLink: React.FC<IListItemProps> = ({to, icon, label, onClick}) => {
+
+    const navigate = useNavigate()
+
+    const resolvedPath = useResolvedPath(to)
+    const match = useMatch({ path: resolvedPath.pathname, end: false})
+
+    const handleClick = () => {
+        navigate(to)
+        onClick?.();
+    }
+
+  return (
+    <ListItemButton selected={!!match} onClick={handleClick}>
+      <ListItemIcon>
+        <Icon>{icon}</Icon>
+      </ListItemIcon>
+      <ListItemText primary={label} />
+    </ListItemButton>
+  );
+};
 
 interface IMenulateralProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export const MenuLateral: React.FC<IMenulateralProps> = ({ children }) => {
-    const theme = useTheme()
-    const smDown = useMediaQuery(theme.breakpoints.down('sm'))
-    const { IsDrawerOpen, toggleDrawerOpen } = useDrawerContext()
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const { IsDrawerOpen, toggleDrawerOpen } = useDrawerContext();
 
-    return (
-        <>
-            <Drawer open={IsDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
-                <Box width={theme.spacing(28)} height="100%" display="flex"flexDirection="column">
+  return (
+    <>
+      <Drawer
+        open={IsDrawerOpen}
+        variant={smDown ? "temporary" : "permanent"}
+        onClose={toggleDrawerOpen}
+      >
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src={imageSrc}
+            />
+          </Box>
 
-                    <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
-                    <Avatar 
-                    sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
-                    src={imageSrc} />
-                    </Box>
+          <Divider />
 
-                    <Divider />
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemLink 
+              icon="home"
+              label="Página inicial"
+              to="/pagina-inicial"
+              onClick={smDown ? toggleDrawerOpen : undefined}
+              />
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
 
-                    <Box flex={1}>
-                        <List component="nav">
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <Icon>home</Icon>
-                                </ListItemIcon>
-                                <ListItemText primary="Página inicial" />
-                            </ListItemButton>
-                        </List>
-                    </Box>
-                </Box>
-            </Drawer>
-        
-        
-            <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
-                {children}
-            </Box>
-        </>
-    )
-}
+      <Box height="100vh" marginLeft={smDown ? 0 : theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
