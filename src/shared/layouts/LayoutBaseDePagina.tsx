f55d912d1c5b4e94627979faa5d28c3a22e511e9
Palombo <dripalombo22@gmail.com>
2024-08-15 17:03:53 -0300

    feat:responsividade in layout

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -1,37 +1,53 @@
 import { Icon, IconButton, useMediaQuery, useTheme } from "@mui/material";
 import { Box, Typography } from "@mui/material";
 import { useDrawerContext } from "../contexts";
+import { ReactNode } from "react";
 
 interface ILayoutProviderProps {
   children: React.ReactNode;
   titulo: string;
+  barraDeFerramentas?: ReactNode
 }
 
 export const LayoutBaseDePagina: React.FC<ILayoutProviderProps> = ({
   children,
   titulo,
+  barraDeFerramentas
 }) => {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const mdDown = useMediaQuery(theme.breakpoints.down("md"));
   const {toggleDrawerOpen} = useDrawerContext()
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
       <Box
         padding={1}
-        height={theme.spacing(8)}
         display="flex"
         alignItems="center"
         gap={1}
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
       >
         {smDown && (<IconButton onClick={toggleDrawerOpen}>
           <Icon>menu</Icon>
         </IconButton>)}
 
-        <Typography variant="h5">{titulo}</Typography>
+        <Typography 
+            whiteSpace='nowrap'
+            overflow='hidden'
+            textOverflow='ellipses'
+            variant={smDown? "h5" : mdDown ? "h4" : "h3" }
+        >
+            {titulo}
+        </Typography>
       </Box>
-      <Box>Barras de ferramentas</Box>
-      <Box>{children}</Box>
+
+      {barraDeFerramentas &&(<Box>
+        {barraDeFerramentas}
+      </Box>)}
+
+      <Box flex={1} overflow='auto'>{children}</Box>
+
     </Box>
   );
 };
